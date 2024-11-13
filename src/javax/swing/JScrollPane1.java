6989e6714a4039620db49ada9a914dae9e6a5132
addin354 <addinhusnannadhari354@gmail.com>
2024-11-13 15:14:40 +0800

    Initial commit

---
+++ src/javax/swing/JScrollPane1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javax.swing;
+
+import java.awt.ScrollPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JScrollPane1 extends ScrollPane {
+    
+}
